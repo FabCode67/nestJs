fae81800f44f0c2c0290d2ff8e912f13bcd759ad
FabCode67 <mwanafunzifabrice@gmail.com>
2024-11-21 11:05:01 +0200

    fix-action-1

--- src/categories/dto/create-category.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsNotEmpty, IsString, MinLength } from 'class-validator';
-
-export class CreateCategoryDto {
-  @IsString()
-  @IsNotEmpty()
-  @MinLength(2, { message: 'Category name must have at least 2 characters.' })
-  name: string;
-}
